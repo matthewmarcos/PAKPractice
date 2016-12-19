707663227b8fa87dbff37ea940385ebc10b41f42
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-20 01:01:02 +0800

    dev server or webpack watch. hmmm

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { render as renderDom } from 'react-dom';
-// import 'materialize-css/bin/materialize.js';
-// import 'materialize-css/bin/materialize.css';
+
 import './index.css';
 
 class App extends Component {
     render() {
         return (
             <div className="app">
-                <h1>Hello from React!!!</h1>
+                <h1>Hello from React!!!!!!</h1>
             </div>
         );
     }
