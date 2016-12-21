e42e88ff997a5c528256f0c28166a4b41387ec47
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-21 17:13:26 +0800

    Add Materialize again. No magic clisterfucker

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { render as renderDom } from 'react-dom';
 
+import 'materialize-css/bin/materialize.js';
+import 'materialize-css/bin/materialize.css';
 import './index.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="app">
+            <div className="app container">
                 <h1>Hello from React!!!!!!</h1>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,10 @@ const path = require('path');
         },{
             test: /\.css$/,
             loader: 'style-loader!css-loader',
-        }]
+        },
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url?limit=10000&mimetype=application/font-woff" },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
+      ]
     }
 };
 
